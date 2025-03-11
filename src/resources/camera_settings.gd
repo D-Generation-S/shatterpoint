edae7d7d78fe7882aaf022e341fae53cea02d989
Xanatos <10531466+XanatosX@users.noreply.github.com>
2025-03-12 00:17:08 +0100

    feat: add basic camera controls
    Add possibility to move camera with keyboard
    Add possibility to move camera by mouse drag
    Add possibility to move camera by edge

---
+++ src/resources/camera_settings.gd
@@ -0,0 +1,7 @@
+class_name CameraSettings extends Resource
+
+@export var mouse_drag_enabled: bool = false
+@export var edge_move_enabled: bool = false
+@export var move_speed: float = 15
+@export var drag_speed: float = 5
+@export var edge_scroll_speed: float = 10
