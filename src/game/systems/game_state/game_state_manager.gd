d2556fcd247f8cc90d11c3879e3ec02cba1f9674
Xanatos <10531466+XanatosX@users.noreply.github.com>
2025-03-25 19:42:58 +0100

    fix: initial wave not spawning

--- src/game/systems/game_state/game_state_manager.gd
+++ src/game/systems/game_state/game_state_manager.gd
@@ -26,7 +26,7 @@ var build_mode_timer: Timer
 var dead_unit_timer: Timer
 var spawners: Array[Spawner]
 var completed_spawners = 0
-var current_wave: int = 0
+var current_wave: int = 1
 
 
 func _ready():
@@ -114,7 +114,7 @@ func build_phase_endet():
 	if !build_mode_timer.is_stopped():
 		build_mode_timer.stop()
 	current_phase = WAVE
-	var message = tr("WAVE_PHASE_STARTED") % current_wave
+	var message = tr("WAVE_PHASE_STARTED") % (current_wave - 1)
 	message_requested.emit(MessagePosition.CENTER, message_style, message, 1.0)
 
 func start_game():
