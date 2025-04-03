1b7dbcf76ec2a7f72f24a46e8f2725391ee39e98
Xanatos <10531466+XanatosX@users.noreply.github.com>
2025-04-03 23:37:33 +0200

    chore: update to Godot 4.4.1
    fix issues with pot translation addon.

--- addons/resource_translation_plugin/plugins/translation_resource_plugin.gd
+++ addons/resource_translation_plugin/plugins/translation_resource_plugin.gd
@@ -7,21 +7,24 @@ func get_plugin_name() -> String:
 func get_extension_name() -> String:
     return "tres"
 
-func _parse_file(path: String, msgids: Array[String], msgids_context_plural: Array[Array]) -> void:
+func _parse_file(path: String) -> Array[PackedStringArray]:
+    var msgids: Array[PackedStringArray] = []
     var res: Resource = load(path)
     if not res:
-        return
+        return msgids
 
     if res is TranslationResource:
         var item: TranslationResource = res as TranslationResource
         if item.key == "":
             printerr("Empty translation in %s" % path)
-            return
+            return msgids
         msgids.append(item.key)
 
     for sub in _sub_parser:
         for data in sub.parse(res):
             msgids.append(data)
+
+    return msgids
 
 func _get_recognized_extensions() -> PackedStringArray:
     return [get_extension_name()]
