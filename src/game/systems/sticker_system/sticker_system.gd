fe4ec8187fedfc5b3b03c6ab82a450a204b4a13a
Xanatos <10531466+XanatosX@users.noreply.github.com>
2025-04-05 23:04:20 +0200

    fix: lagspike on particle system start

--- src/game/systems/sticker_system/sticker_system.gd
+++ src/game/systems/sticker_system/sticker_system.gd
@@ -9,3 +9,7 @@ class_name StickerSystem extends Node
 func request_sticker_at_position(global_sticker_position: Vector2, icon: Texture):
 	var sticker = Sticker.new(global_sticker_position, icon, randf_range(min_time_to_life, max_time_to_life), start_alpha)
 	root_sticker_node.add_child(sticker)
+
+func clear_stickers():
+	for sticker in root_sticker_node.get_children():
+		sticker.queue_free()
---
+++ src/ui/loading_overlay.gd
@@ -0,0 +1,4 @@
+extends CanvasLayer
+
+func loading_done():
+    queue_free()
