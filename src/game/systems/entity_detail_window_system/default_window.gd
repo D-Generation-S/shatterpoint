4c2f122c564220315e81f27ed938165fb68861a5
Xanatos <10531466+XanatosX@users.noreply.github.com>
2025-04-11 21:42:47 +0200

    fix: popup window works correctly now
    The popup window if you click on a building had issues with the new
    explanation code. This is fixed now.

--- src/game/systems/entity_detail_window_system/default_window.gd
+++ src/game/systems/entity_detail_window_system/default_window.gd
@@ -1,4 +1,7 @@
-class_name DefaultWindow extends Window
+class_name DefaultWindow extends PanelContainer
+
+signal close_requested()
+signal title_changed(text: String)
 
 @export var content_target_node: Control
 
@@ -8,13 +11,13 @@ var planned_content: Array[DefaultDetailContent]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	title = planned_title
+	title_changed.emit(planned_title)
 	if planned_content.size() == 1:
 		add_child(planned_content[0])
 	else:
 		add_multi_content(planned_content)
 
-	popup_centered(planned_position_and_size.size)
+	update_position()
 
 
 func add_multi_content(content: Array[DefaultDetailContent]) -> void:
@@ -28,12 +31,17 @@ func add_multi_content(content: Array[DefaultDetailContent]) -> void:
 	content_target_node.add_child(tab_container)
 
 func setup(position_and_size: Rect2, new_title: String, content: Array[DefaultDetailContent]) -> void:
-	planned_title = new_title
-	planned_position_and_size = position_and_size
-	planned_content = content
 	
+	planned_position_and_size = position_and_size
+	planned_title = new_title
+	planned_content = content
 
+	custom_minimum_size = planned_position_and_size.size
 
+func update_position():
+	var viewport_size = get_viewport_rect().size
+	position = Vector2(viewport_size.x / 2, viewport_size.y / 2)
+	position -= planned_position_and_size.size / 2
 
 func request_close():
 	close_requested.emit()
